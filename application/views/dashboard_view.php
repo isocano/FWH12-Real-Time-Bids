d7f2a0a6dd15847b944e8ea17e447c6c57c92bd8
Alex <alex@saldum.com>
2012-09-18 16:38:51 +0200

    dash

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -1,7 +1,13 @@
 <div class="row">
 
-	<div class="five columns"><a href="#"><p>Logo<p></div>
-	<div class="seven columns"><a href="#">Add new auction</a></div>
+	<div class="five columns"><a href="#" style="color: white;"><p>Logo<p></div>
+	<div class="seven columns"><a href="#" style="color: white;">Add new auction</a></div>
 	
+
+</div>
+
+<div class="row">
+
+	<div class="twelve columns" style="min-height: 500px; min-width: 650px";>
 
 </div>
