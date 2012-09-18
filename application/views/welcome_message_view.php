3993c85c34861a0fa5820e191452024284589030
Alex <alex@saldum.com>
2012-09-18 16:27:33 +0200

    Facebook button (centrar)

--- application/views/welcome_message_view.php
+++ application/views/welcome_message_view.php
@@ -1,12 +1,15 @@
 <div class="row">
 	<div class="twelve columns">
 		<h1>Welcome to Livebidr!</h1>
-		<div class="twelve columns centered"><a href="#">
-			<img src="https://dl.dropbox.com/u/35700385/livebidr/logo.png"></a>
+		<div class="twelve columns centered" style="text-align: center;">
+			<a href="#"><img src="https://dl.dropbox.com/u/35700385/livebidr/logo.png"></a>
 		</div>
+		
+		<div class="twelve columns centered" style="text-align: center;">
 		<?php if ( ! $this->session->userdata('user_id')) { ?>
 	    	<?php echo $this->feisbus->get_login_button(TRUE, 'xlarge'); ?>
 	    <?php } ?>  
 	    <?php echo $this->feisbus->load_javascript_SDK(); ?>
+	    </div>
 	</div>
 </div>
