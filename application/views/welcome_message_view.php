7c0416d512a27890c1099de99924142d854cfe9d
Alex <alex@saldum.com>
2012-09-18 16:24:56 +0200

    Centrar

--- application/views/welcome_message_view.php
+++ application/views/welcome_message_view.php
@@ -1,7 +1,9 @@
 <div class="row">
 	<div class="twelve columns">
 		<h1>Welcome to Livebidr!</h1>
-		<a href="#"><img src="https://dl.dropbox.com/u/35700385/livebidr/logo.png"></a>
+		<div class="twelve columns centered"><a href="#">
+			<img src="https://dl.dropbox.com/u/35700385/livebidr/logo.png"></a>
+		</div>
 		<?php if ( ! $this->session->userdata('user_id')) { ?>
 	    	<?php echo $this->feisbus->get_login_button(TRUE, 'xlarge'); ?>
 	    <?php } ?>  
