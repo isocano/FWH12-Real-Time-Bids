7d5244cf72406e0b6f9a4277aec8e814927ba717
Ignacio <ignacio@saldum.com>
2012-09-18 19:17:02 +0200

    fdsfdsf

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -2,13 +2,12 @@
 	<div class="five columns"><a href="#"><img src="https://dl.dropbox.com/u/35700385/livebidr/logo-mini.png" style="margin-bottom: 15px;"></a></div>
 	<div class="seven columns" style="text-align: right;"><a href="<?php echo $this->config->item('base_url'); ?>auction/add" style="color: white;">Add new auction</a></div>
 </div>
-<div class="row">
+<div class="row" style="min-height: 500px; min-width: 650px; background-color: white; border: 1px solid #7b7b7b; border-radius: 4px; margin-bottom: 15px;">
 	<div class="eight columns" style="background-color: #f6f6f6; text-align: center;">
 		<p>Hello <?php echo $first_name?></p>
 		<img src="<?php echo "http://graph.facebook.com/$user_id/picture?type=square";?>"/>
 	</div>
-</div>
-<div class="row" style="min-height: 500px; min-width: 650px; background-color: white; border: 1px solid #7b7b7b; border-radius: 4px; margin-bottom: 15px;">
+	<br/>
 	<div class="eight columns" style="background-color: #f6f6f6; text-align: center;">
 		<a href="/auction"><p style="font-size: 22px;">Awesome World Globe</p></a>
 	<div>
