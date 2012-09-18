1f35d23c36f17298fa62f53eda7bd6629d5db14c
Ignacio <ignacio@saldum.com>
2012-09-18 15:29:32 +0200

    Fix pusher

--- application/views/auction_view.php
+++ application/views/auction_view.php
@@ -1,6 +1,6 @@
 <?php 
 	// https://github.com/squeeks/Pusher-PHP
-	require('../libraries/Pusher/Pusher.php');
+	require('/var/www/html/FWH12-Real-Time-Bids/application/libraries/Pusher/lib/Pusher.php');
 
 	$key = '131de32e0bed65790199';
 	$secret = '69d5b8a4d4f0696b3c7a';
@@ -10,11 +10,11 @@
 	$pusher->trigger('my-channel', 'my-event', array('message' => 'hello world') );
 ?>
 
-<scipt>
+<script>
 	var pusher = new Pusher('131de32e0bed65790199'); // Replace with your app key
 	var channel = pusher.subscribe('my-channel');
 	
 	channel.bind('my-event', function(data) {
 	  alert('An event was triggered with message: ' + data.message);
 	});
-</scipt>
+</script>
