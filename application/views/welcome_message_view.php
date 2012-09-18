5dc05ae422a179f69962f347508b5320578c6653
Alex <alex@saldum.com>
2012-09-18 16:19:07 +0200

    Cambios CSS

--- application/views/welcome_message_view.php
+++ application/views/welcome_message_view.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="twelve columns">
-		<h1>Welcome to live bidr!</h1>
+		<h1>Welcome to Livebidr!</h1>
 		<?php if ( ! $this->session->userdata('user_id')) { ?>
 	    	<?php echo $this->feisbus->get_login_button(TRUE, 'xlarge'); ?>
 	    <?php } ?>  
