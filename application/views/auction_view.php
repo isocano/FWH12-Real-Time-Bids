1d31625c4d9c08128d1d6aa7acf939cd34ee065f
Ignacio <ignacio@saldum.com>
2012-09-18 15:11:09 +0200

    Fixing…

--- application/views/auction_view.php
+++ application/views/auction_view.php
@@ -1,20 +1,21 @@
 <?php 
+echo 'hola';
 	// https://github.com/squeeks/Pusher-PHP
-	require('../libraries/Pusher/Pusher.php');
+	/*require('../libraries/Pusher/Pusher.php');
 
 	$key = '131de32e0bed65790199';
 	$secret = '69d5b8a4d4f0696b3c7a';
 	$app_id = '27903';
 
 	$pusher = new Pusher($key, $secret, $app_id);
-	$pusher->trigger('my-channel', 'my-event', array('message' => 'hello world') );
+	$pusher->trigger('my-channel', 'my-event', array('message' => 'hello world') );*/
 ?>
 
-<scipt>
+<!--<scipt>
 	var pusher = new Pusher('131de32e0bed65790199'); // Replace with your app key
 	var channel = pusher.subscribe('my-channel');
 	
 	channel.bind('my-event', function(data) {
 	  alert('An event was triggered with message: ' + data.message);
 	});
-</scipt>
+</scipt>-->
