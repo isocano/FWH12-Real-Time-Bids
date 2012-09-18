357f333582d3e18ddea40194a814d11542d624b6
Ignacio <ignacio@saldum.com>
2012-09-18 16:12:24 +0200

    remove index.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://sldm.co/index.php/';
+$config['base_url']	= 'http://sldm.co/';
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ $config['base_url']	= 'http://sldm.co/index.php/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
