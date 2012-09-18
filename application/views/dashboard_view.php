8a710e1dcc8860d19107f7237ae2140b6d13a40b
Pablo Sánchez <pasaloray@hotmail.com>
2012-09-18 19:17:12 +0200

    no message

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -9,8 +9,9 @@
 <div class="row" style="min-height: 500px; min-width: 650px; background-color: white; border: 1px solid #7b7b7b; border-radius: 4px; margin-bottom: 15px;">
 
 	<div class="eight columns" style="background-color: #f6f6f6; text-align: center;";>
-
+		<h3>Our auctions</h3>
 		<a href="/auction"><p style="font-size: 22px;">Awesome World Globe</p></a>
+		<img src="http://sldm.co/data/lot/1/globe.png" width="250"/> 
 		
 	<div>
 		
