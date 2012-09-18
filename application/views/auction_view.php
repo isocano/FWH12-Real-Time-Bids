61e13d0490cd68f4076c786017a7c0d0038acd44
Ignacio <ignacio@saldum.com>
2012-09-18 17:18:49 +0200

    countdown

--- application/views/auction_view.php
+++ application/views/auction_view.php
@@ -11,6 +11,7 @@
 ?>
 
 <div class="row">
+	<div id="javascript_countdown_time"></div>
 	<p>Puja acutal:</p><span id="actual_bid">10</span>
 	<a href="#" id="bid">10</a> + 5
 	<div class="twelve columns">
@@ -32,11 +33,88 @@
 	});
 	
 	$("#bid").click(function() {
-		var msg = $('#bid').html() + 5;
+		var msg = parseInt($('#bid').html()) + 5;
   		$.ajax({
 		  type: "GET",
 		  url: base_url + "auction/push/" + msg,
 		}).done(function( msg ) {
 		});
 	});
+</script>
+
+<script>
+	var javascript_countdown = function () {
+	var time_left = 10; //number of seconds for countdown
+	var output_element_id = 'javascript_countdown_time';
+	var keep_counting = 1;
+	var no_time_left_message = 'No time left for JavaScript countdown!';
+ 
+	function countdown() {
+		if(time_left < 2) {
+			keep_counting = 0;
+		}
+ 
+		time_left = time_left - 1;
+	}
+ 
+	function add_leading_zero(n) {
+		if(n.toString().length < 2) {
+			return '0' + n;
+		} else {
+			return n;
+		}
+	}
+ 
+	function format_output() {
+		var hours, minutes, seconds;
+		seconds = time_left % 60;
+		minutes = Math.floor(time_left / 60) % 60;
+		hours = Math.floor(time_left / 3600);
+ 
+		seconds = add_leading_zero( seconds );
+		minutes = add_leading_zero( minutes );
+		hours = add_leading_zero( hours );
+ 
+		return hours + ':' + minutes + ':' + seconds;
+	}
+ 
+	function show_time_left() {
+		document.getElementById(output_element_id).innerHTML = format_output();//time_left;
+	}
+ 
+	function no_time_left() {
+		document.getElementById(output_element_id).innerHTML = no_time_left_message;
+	}
+ 
+	return {
+		count: function () {
+			countdown();
+			show_time_left();
+		},
+		timer: function () {
+			javascript_countdown.count();
+ 
+			if(keep_counting) {
+				setTimeout("javascript_countdown.timer();", 1000);
+			} else {
+				no_time_left();
+			}
+		},
+		//Kristian Messer requested recalculation of time that is left
+		setTimeLeft: function (t) {
+			time_left = t;
+			if(keep_counting == 0) {
+				javascript_countdown.timer();
+			}
+		},
+		init: function (t, element_id) {
+			time_left = t;
+			output_element_id = element_id;
+			javascript_countdown.timer();
+		}
+	};
+}();
+ 
+//time to countdown in seconds, and element ID
+javascript_countdown.init(3673, 'javascript_countdown_time');
 </script>
